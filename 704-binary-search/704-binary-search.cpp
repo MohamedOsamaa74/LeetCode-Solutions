6532020f31e5d1f49a3659741ecd2ec98a1d2737
Mohamed Osama <73244389+momoosos@users.noreply.github.com>
2022-06-30 16:49:00 +0200

    Time: 62 ms (35.44%), Space: 27.6 MB (12.48%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st = 0, en = nums.size()-1, md;
+        while(en>=st){
+            md = st+en >> 1;
+            if(nums[md]==target) return md;
+            else if(nums[md]>target) en = md-1;
+            else st = md+1;
+        }
+        return -1;
+    }
+};
