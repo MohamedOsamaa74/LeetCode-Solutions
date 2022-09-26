209b8db1662d6dcf1b6e9495dca1f43ccae01a8a
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2022-09-26 22:23:59 +0200

    Time: 0 ms (100.00%), Space: 5.7 MB (87.83%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool isSameAfterReversals(int n) {
-        bool ok = 0;
-        while(n%10==0 && n!=0){
-            n/=10;
-            ok = 1;
-        }
-        if(!ok) return true;
-        if(n!=0 && !ok) return true;
-        return false;
+        return n==0 || n%10;
     }
 };
