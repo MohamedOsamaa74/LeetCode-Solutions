a780f25fc99f1c1fbe0aef321a628f67f2d3506a
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2022-09-26 20:05:11 +0200

    Time: 3 ms (66.35%), Space: 8.8 MB (61.57%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -2,28 +2,15 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        if(digits[n-1]!=9){
-            digits[n-1]++;
-        }
-        else{
-            set<int>st(digits.begin(), digits.end());
-            if(st.size()==1){
-                digits[0]=1;
-                for(int i = 1 ; i < n ; i++){
-                    digits[i]=0;
-                }
-                digits.push_back(0);
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
             }
-            else{
-                for(int i = n-1 ; i >= 0 ; i--){
-                    if(digits[i]==9) digits[i]=0;
-                    else{
-                        digits[i]++;
-                        break;
-                    }
-                }
-            }
+            digits[i] = 0;
         }
-        return digits;
+        vector<int>ans(n+1);
+        ans[0]=1;
+        return ans;
     }
 };
