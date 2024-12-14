03bc5a88a0c2f0a597962c3dfed35475e7200f06
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2024-12-14 20:13:38 +0200

    [217] [Time Beats: 71.55%] [Memory Beats: 65.05%] - LeetPush

--- 217-Contains-Duplicate.cpp
+++ 217-Contains-Duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int>mp;
+        unordered_map<int, int>mp;
         for(int num : nums){
             if(mp[num]>=1)
                 return true;
