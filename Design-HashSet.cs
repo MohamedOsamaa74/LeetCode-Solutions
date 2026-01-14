702fdf3acdce75f91701a554791b19fc6676e395
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-01-14 15:54:41 +0200

    [705] [Time Beats: 97.21%] [Memory Beats: 19.07%] - LeetPush

--- Design-HashSet.cs
+++ Design-HashSet.cs
@@ -1,19 +1,19 @@
 1public class MyHashSet {
-2    private List<int> data;
+2    private bool[] data;
 3    public MyHashSet() {
-4        data = new List<int>();
+4        data = new bool[1000001];
 5    }
 6    
 7    public void Add(int key) {
-8        if(!data.Contains(key)) data.Add(key);
+8        data[key] = true;
 9    }
 10    
 11    public void Remove(int key) {
-12        if(data.Contains(key)) data.Remove(key);
+12        data[key] = false;
 13    }
 14    
 15    public bool Contains(int key) {
-16        return data.Contains(key);
+16        return data[key];
 17    }
 18}
 19
