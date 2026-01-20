c64fc4b0255f6f8711db2d858abb5f39af2e6bf2
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-01-20 16:46:44 +0200

    [1752] [Time Beats: 100.00%] [Memory Beats: 97.49%] - LeetPush

--- General/Check-if-Array-Is-Sorted-and-Rotated.cs
+++ General/Check-if-Array-Is-Sorted-and-Rotated.cs
@@ -1,19 +1,10 @@
 1public class Solution {
 2    public bool Check(int[] nums) {
-3        int[] sortedArray = (int[]) nums.Clone();
-4        Array.Sort(sortedArray);
-5        int x = 0;
-6        while(x <= nums.Length){
-7            bool ok = true;
-8            for(int i = 0; i < nums.Length; i++){
-9                if(nums[i]!=sortedArray[(i+x) % nums.Length]){
-10                    ok = false;
-11                    break;
-12                }
-13            }
-14            if(ok) return true;
-15            x++;
-16        }
-17        return false;
-18    }
-19}
+3        int cnt = 0;
+4        for(int i = 1; i < nums.Length; i++){
+5            if(nums[i]<nums[i-1]) cnt++;
+6        }
+7        if(nums[0]<nums[nums.Length-1]) cnt++;
+8        return cnt<=1;
+9    }
+10}
