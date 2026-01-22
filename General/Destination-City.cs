fcf2850279cb71b34d966f428b8b3b68b5a62890
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-01-22 21:03:56 +0200

    [1436] [Time Beats: 96.67%] [Memory Beats: 71.11%] - LeetPush

--- General/Destination-City.cs
+++ General/Destination-City.cs
@@ -1,12 +1,12 @@
 1public class Solution {
 2    public string DestCity(IList<IList<string>> paths) {
-3        Dictionary<string, string> mp = new();
+3        HashSet<string> st = new();
 4        string ans = "";
 5        foreach(var path in paths){
-6            mp[path[0]] = path[1];
+6            st.Add(path[0]);
 7        }
 8        foreach(var path in paths){
-9            if(!mp.ContainsKey(path[1])) ans = path[1];
+9            if(!st.Contains(path[1])) ans = path[1];
 10        }
 11        return ans;
 12    }
