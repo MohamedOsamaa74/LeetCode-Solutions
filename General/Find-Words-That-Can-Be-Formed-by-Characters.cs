e0d0d240d45e672ed5ac62fee38db29b3a1abba3
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-01-22 18:50:04 +0200

    [1160] [Time Beats: 99.01%] [Memory Beats: 68.32%] - LeetPush

--- General/Find-Words-That-Can-Be-Formed-by-Characters.cs
+++ General/Find-Words-That-Can-Be-Formed-by-Characters.cs
@@ -1,24 +1,22 @@
 1public class Solution {
 2    public int CountCharacters(string[] words, string chars) {
-3        Dictionary<char, int> mp = new();
+3        int[] mp = new int[26];
 4        foreach(char ch in chars){
-5            if(mp.ContainsKey(ch)) mp[ch]++;
-6            else mp[ch] = 1;
-7        }
-8        int ans = 0;
-9        foreach(var word in words){
-10            Dictionary<char, int> freq = new();
-11            bool ok = true;
-12            foreach(var ch in word){
-13                if(freq.ContainsKey(ch)) freq[ch]++;
-14                else freq[ch] = 1;
-15                if(!mp.ContainsKey(ch) || (mp.ContainsKey(ch) && freq[ch] > mp[ch])){
-16                    ok = false;
-17                    break;
-18                } 
-19            }
-20            if(ok) ans+=word.Length;
-21        }
-22        return ans;
-23    }
-24}
+5            mp[ch-'a']++;
+6        }
+7        int ans = 0;
+8        foreach(var word in words){
+9            int[] freq = new int[26];
+10            bool ok = true;
+11            foreach(var ch in word){
+12                freq[ch-'a']++;
+13                if(mp[ch-'a']==0 || (freq[ch-'a'] > mp[ch-'a'])){
+14                    ok = false;
+15                    break;
+16                } 
+17            }
+18            if(ok) ans+=word.Length;
+19        }
+20        return ans;
+21    }
+22}
