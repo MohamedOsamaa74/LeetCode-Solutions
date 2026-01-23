f22eeba79f2cd79100f309a86c4946966041ae76
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-01-23 15:46:01 +0200

    [1913] [Time Beats: 92.00%] [Memory Beats: 6.00%] - LeetPush

--- General/Maximum-Product-Difference-Between-Two-Pairs.cs
+++ General/Maximum-Product-Difference-Between-Two-Pairs.cs
@@ -1,7 +1,22 @@
 1public class Solution {
 2    public int MaxProductDifference(int[] nums) {
-3        Array.Sort(nums);
-4        int n = nums.Length;
-5        return (nums[n-1]*nums[n-2]) - (nums[0]*nums[1]);
-6    }
-7}
+3        int mx1 = 0, mx2 = 0, mn1 = 100005, mn2 = 100005;
+4        foreach(int x in nums){
+5            if(x > mx1){
+6                mx2 = mx1;
+7                mx1 = x;
+8            }
+9            else if(x > mx2){
+10                mx2 = x;
+11            }
+12            if(x < mn1){
+13                mn2 = mn1;
+14                mn1 = x;
+15            }
+16            else if(x < mn2){
+17                mn2 = x;
+18            }
+19        }
+20        return mx1*mx2 - mn1*mn2;
+21    }
+22}
