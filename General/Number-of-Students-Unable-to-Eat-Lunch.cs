57b81441a6f27d1bac3a9c4bc99f658fb7701819
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-02-23 22:49:20 +0200

    [1700] [Time Beats: 29.75%] [Memory Beats: 48.77%] - LeetPush

--- General/Number-of-Students-Unable-to-Eat-Lunch.cs
+++ General/Number-of-Students-Unable-to-Eat-Lunch.cs
@@ -2,24 +2,26 @@
 2    public int CountStudents(int[] students, int[] sandwiches) {
 3        Queue<int> queue = new();
 4        Stack<int> st = new();
-5        foreach (int s in students) queue.Enqueue(s);
-6        for (int i = sandwiches.Length - 1; i >= 0; i--) st.Push(sandwiches[i]);
-7        int rot = 0;
-8        while (st.Count>0)
-9        {
-10            if (queue.Peek() == st.Peek())
-11            {
-12                queue.Dequeue();
-13                st.Pop();
-14                rot = 0;
-15            }
-16            else
-17            {
-18                queue.Enqueue(queue.Dequeue());
-19                rot++;
-20            }
-21            if(rot == queue.Count) break;
-22        }
-23        return rot;
-24    }
-25}
+5        for (int i = sandwiches.Length - 1; i >= 0; i--){
+6            st.Push(sandwiches[i]);
+7            queue.Enqueue(students[i]);
+8        }
+9        int rot = 0;
+10        while (st.Count>0)
+11        {
+12            if (queue.Peek() == st.Peek())
+13            {
+14                queue.Dequeue();
+15                st.Pop();
+16                rot = 0;
+17            }
+18            else
+19            {
+20                queue.Enqueue(queue.Dequeue());
+21                rot++;
+22            }
+23            if(rot == queue.Count) break;
+24        }
+25        return rot;
+26    }
+27}
