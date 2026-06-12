451e4578461cda9eae82b53286a306ec0c4b7454
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-06-13 01:59:06 +0300

    [26] [Time Beats: 100.00%] [Memory Beats: 77.60%] - LeetPush

--- General/Remove-Duplicates-from-Sorted-Array.cs
+++ General/Remove-Duplicates-from-Sorted-Array.cs
@@ -1,11 +1,13 @@
 1public class Solution {
 2    public int RemoveDuplicates(int[] nums) {
-3        SortedSet<int> st = new();
-4        foreach(int i in nums) st.Add(i);
-5        for(int i = 0; i < nums.Length; i++){
-6            if(i+1<=st.Count) nums[i] = st.ElementAt(i);
-7        }
-8        Array.Resize(ref nums, st.Count);
-9        return nums.Length;
-10    }
-11}
+3        if(nums.Length == 0) return 0;
+4        int j = 0;
+5        for(int i = 1; i < nums.Length; i++)
+6        {
+7            if(nums[i] != nums[j]){
+8                nums[++j] = nums[i];
+9            }
+10        }
+11        return j+1;
+12    }
+13}
