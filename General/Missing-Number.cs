2cd3dcca9d9ce4f84ffba63f996c602a5592bc42
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-06-17 06:14:28 +0300

    [268] [Time Beats: 100.00%] [Memory Beats: 72.18%] - LeetPush

--- General/Missing-Number.cs
+++ General/Missing-Number.cs
@@ -1,14 +1,9 @@
 1public class Solution {
 2    public int MissingNumber(int[] nums) {
-3        bool[] arr = new bool[nums.Length+1];
-4        foreach(var i in nums)
-5        {
-6            arr[i] = true;
-7        }
-8        for(int i = 0; i <= nums.Length; i++)
-9        {
-10            if(arr[i]==false) return i;
-11        }
-12        return -1;
-13    }
-14}
+3        int n = nums.Length;
+4        int expectedSum = n * (n + 1) / 2;
+5        int sum = 0;
+6        foreach(var i in nums) sum+=i;
+7        return expectedSum-sum;
+8    }
+9}
