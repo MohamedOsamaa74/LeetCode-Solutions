bb37d7033caf3c8c366d02f75c070609cc1292fc
Mohamed Osama <73244389+MohamedOsamaa74@users.noreply.github.com>
2026-06-17 05:44:14 +0300

    [217] [Time Beats: 86.69%] [Memory Beats: 80.38%] - LeetPush

--- General/Contains-Duplicate.cs
+++ General/Contains-Duplicate.cs
@@ -1,12 +1,5 @@
 1public class Solution {
 2    public bool ContainsDuplicate(int[] nums) {
-3        HashSet<int> st = new();
-4        foreach(var i in nums)
-5        {
-6            int n = st.Count;
-7            st.Add(i);
-8            if(st.Count == n) return true;
-9        }
-10        return false;
-11    }
-12}
+3        return new HashSet<int>(nums).Count < nums.Length;
+4    }
+5}
